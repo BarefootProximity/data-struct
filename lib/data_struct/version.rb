4f6991550b390dbd31a3a9b00cda88ecab77370a
Scott Nelson <snelson@barefootproximity.com>
2018-03-28 14:53:57 -0400

    Ensure Version can be read before bundle is installed.

--- lib/data_struct/version.rb
+++ lib/data_struct/version.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-require 'recursive-open-struct'
+# We need to ensure OpenStruct and RecursiveOpenStruct classes exist to extend from
+# Before the bundle is installed.
+# Otherwise, the build fails when trying to read the version
+require 'ostruct'
+
+class RecursiveOpenStruct < OpenStruct
+end
 
 class DataStruct < RecursiveOpenStruct
   VERSION = '0.1.0'
